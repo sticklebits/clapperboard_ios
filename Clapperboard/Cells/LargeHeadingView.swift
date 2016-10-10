3334b5183e3ebe9ece4759f9eaf979be9c1d6aa6
Steve Johnson <steve@brightec.co.uk>
2016-10-10 14:51:21 +0100

    newlook: Added recent search header.

---
+++ Clapperboard/Cells/LargeHeadingView.swift
@@ -0,0 +1,21 @@
+//
+//  LargeHeadingView.swift
+//  Clapperboard
+//
+//  Created by Steve Johnson on 10/10/2016.
+//  Copyright © 2016 Sticklebits. All rights reserved.
+//
+
+import UIKit
+
+class LargeHeadingView: UIView {
+
+    @IBOutlet weak var headingLabel: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    var onTouched: ((_ button: UIButton) -> (Void))?
+    
+    @IBAction func actionButtonWasTouched(_ sender: UIButton) {
+        onTouched?(sender)
+    }
+}
--- Clapperboard/Controllers/SearchViewController.swift
+++ Clapperboard/Controllers/SearchViewController.swift
@@ -13,10 +13,11 @@ class SearchViewController: UIViewController {
     enum CollectionViewSections: String {
         case searchHeader
         case searchResults
+        case recentSearchesHeader
         case recentSearches
         
         static func allSections() -> [CollectionViewSections] {
-            return [.searchHeader, .searchResults, .recentSearches]
+            return [.searchHeader, .searchResults, .recentSearchesHeader, .recentSearches]
         }
     }
     
@@ -24,6 +25,7 @@ class SearchViewController: UIViewController {
     
     let SearchHeaderIdentifier = "SearchHeaderIdentifier"
     let SearchResultsIdentifier = "SearchResultsIdentifier"
+    let RecentSearchesHeaderIdentifier = "RecentSearchesHeaderIdentifier"
     let RecentSearchIdentifier = "RecentSearchIdentifier"
     
     var collectionView: UICollectionView!
@@ -57,6 +59,7 @@ class SearchViewController: UIViewController {
         collectionView.delegate = self
         collectionView.register(UINib(nibName:"SearchHeaderCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: SearchHeaderIdentifier)
         collectionView.register(UINib(nibName:"MovieCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: SearchResultsIdentifier)
+        collectionView.register(UICollectionViewCell.classForCoder(), forCellWithReuseIdentifier: RecentSearchesHeaderIdentifier)
         collectionView.register(UICollectionViewCell.classForCoder(), forCellWithReuseIdentifier: RecentSearchIdentifier)
         view.addSubview(collectionView)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -98,6 +101,7 @@ extension SearchViewController : UICollectionViewDataSource {
         switch (collectionViewSection) {
         case .searchHeader: return 1
         case .searchResults: return searchResults.count
+        case .recentSearchesHeader: return 1
         case .recentSearches: return recentSearches.count
         }
     }
@@ -126,12 +130,36 @@ extension SearchViewController : UICollectionViewDataSource {
                 return cell
             }
             
+        case .recentSearchesHeader:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecentSearchesHeaderIdentifier, for: indexPath)
+            cell.subviews.forEach({ (view) in
+                view.removeFromSuperview()
+            })
+            
+            let headerView = UINib(nibName: "LargeHeadingView", bundle: nil).instantiate(withOwner: self, options: nil).first as! LargeHeadingView
+            cell.addSubview(headerView)
+            headerView.translatesAutoresizingMaskIntoConstraints = false
+            cell.addConstraint(NSLayoutConstraint(item: headerView, attribute: .leading, relatedBy: .equal, toItem: cell, attribute: .leading, multiplier: 1.0, constant: 0.0))
+            cell.addConstraint(NSLayoutConstraint(item: headerView, attribute: .trailing, relatedBy: .equal, toItem: cell, attribute: .trailing, multiplier: 1.0, constant: 0.0))
+            cell.addConstraint(NSLayoutConstraint(item: headerView, attribute: .top, relatedBy: .equal, toItem: cell, attribute: .top, multiplier: 1.0, constant: 0.0))
+            cell.addConstraint(NSLayoutConstraint(item: headerView, attribute: .bottom, relatedBy: .equal, toItem: cell, attribute: .bottom, multiplier: 1.0, constant: 0.0))
+            
+            headerView.headingLabel.text = "Recent Searches"
+            headerView.actionButton.setTitle("Clear", for: .normal)
+
+            return cell
+            
         case .recentSearches:
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecentSearchIdentifier, for: indexPath)
             let label = UILabel()
             label.textAlignment = .left
             label.text = recentSearches[indexPath.row]
             cell.contentView.addSubview(label)
+            label.translatesAutoresizingMaskIntoConstraints = false
+            cell.addConstraint(NSLayoutConstraint(item: label, attribute: .leading, relatedBy: .equal, toItem: cell, attribute: .leading, multiplier: 1.0, constant: 0.0))
+            cell.addConstraint(NSLayoutConstraint(item: label, attribute: .trailing, relatedBy: .equal, toItem: cell, attribute: .trailing, multiplier: 1.0, constant: 0.0))
+            cell.addConstraint(NSLayoutConstraint(item: label, attribute: .top, relatedBy: .equal, toItem: cell, attribute: .top, multiplier: 1.0, constant: 0.0))
+            cell.addConstraint(NSLayoutConstraint(item: label, attribute: .bottom, relatedBy: .equal, toItem: cell, attribute: .bottom, multiplier: 1.0, constant: 0.0))
             return cell
         }
         return UICollectionViewCell()
@@ -145,9 +173,10 @@ extension SearchViewController: UICollectionViewDelegateFlowLayout {
         let collectionViewSection = CollectionViewSections.allSections()[indexPath.section]
         
         switch (collectionViewSection) {
-        case .searchHeader: return CGSize(width: collectionView.bounds.width, height: 160.0)
+        case .searchHeader: return CGSize(width: collectionView.bounds.width - 12.0, height: 160.0)
         case .searchResults: return CGSize(width: 180.0, height: 320.0)
-        case .recentSearches: return CGSize(width: collectionView.bounds.width, height: 48.0)
+        case .recentSearchesHeader: return CGSize(width: collectionView.bounds.width - 12.0, height: 48.0)
+        case .recentSearches: return CGSize(width: collectionView.bounds.width - 12.0, height: 48.0)
         }
     }
 }
