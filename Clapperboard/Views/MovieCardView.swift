19950fac85cf118350c7f4293cf0b2205879079e
Steve Johnson <Steve@steves-mbp-2.home>
2016-10-16 17:16:22 +0100

    master: Added Movie Card View

---
+++ Clapperboard/Views/MovieCardView.swift
@@ -0,0 +1,21 @@
+//
+//  MovieCardView.swift
+//  Clapperboard
+//
+//  Created by Steve Johnson on 16/10/2016.
+//  Copyright © 2016 Sticklebits. All rights reserved.
+//
+
+import UIKit
+
+class MovieCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
